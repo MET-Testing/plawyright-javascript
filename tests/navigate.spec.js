4cffaa82cf8b3a8f060a1e328f2f85970d603f94
Dash <db.quiroga93@gmail.com>
2024-10-20 13:51:22 -0300

    se añade una spec de pruebas, se modifica readme y se cambia la config

---
+++ tests/navigate.spec.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@playwright/test')
+
+test('Navigate to Home Page', async ({ page }) => {
+	// Navega a la página de inicio usando la baseURL
+	await page.goto('/') // Esto navega a 'https://www.saucedemo.com/'
+
+	// Verifica el título de la página
+	const title = await page.title()
+	expect(title).toBe('Swag Labs')
+})
